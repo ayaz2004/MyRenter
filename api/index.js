65551d705d3039a76064d84410ba074e7379f399
ayaz2004 <ayaznazir03@gmail.com>
2024-03-31 02:27:06 +0530

    Completed create listing page functionality and minor changes in index.js and user.model.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/app/listing", listingRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
