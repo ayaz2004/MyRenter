af37c99ceb9bc0d5c3d9b2760c510494a536c385
ayaz2004 <ayaznazir03@gmail.com>
2024-04-20 14:27:13 +0530

    Completed Listing Page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -280,7 +280,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">( Rs. / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">(Rs / month)</span>
+                )}
               </div>
             </div>
 
@@ -298,7 +300,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">( Rs. / month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">(Rs / month)</span>
+                  )}
                 </div>
               </div>
             )}
