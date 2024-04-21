8aa1fd629b27b278a41c388fe30d857a99fbf755
ayaz2004 <ayaznazir03@gmail.com>
2024-04-21 13:56:31 +0530

    Completed About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl">
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">About MyRenter</h1>
+      <p classname="mb-4 text-slate-700">Details1</p>
+      <p classname="mb-4 text-slate-700">Details2</p>
+      <p classname="mb-4 text-slate-700">Details3</p>
+    </div>
+  );
 }
