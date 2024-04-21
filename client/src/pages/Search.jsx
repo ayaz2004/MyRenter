e24ae917674ac40809c0d42b79b9b0cb9fc50550
ayaz2004 <ayaznazir03@gmail.com>
2024-04-21 13:41:25 +0530

    Completed Home page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ListingItem from "./ListingItem";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
